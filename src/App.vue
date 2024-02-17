bb7634f87a8f7563ad6a5712550ee619ed5856e1
Ethan <nightwish.ethan@gmail.com>
2024-02-18 00:35:21 +0800

    新建项目 初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<!--  <img alt="Vue logo" src="./assets/logo.png" />-->
+<!--  <HelloWorld msg="Hello Vue 3.0 + Vite" />-->
+  <div>hi</div>
+  <frank></frank>
+</template>
+
+<script>
+import frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    frank
+  }
+}
+</script>
---
+++ src/components/Frank.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    我的第一个组件
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      data1: null
+    }
+  },
+  methods: {
+    method1() {
+    }
+  }
+}
+</script>
