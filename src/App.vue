ee1cf74a12832006ff40fa62c82cfa79d7024a4a
Ethan <nightwish.ethan@gmail.com>
2024-02-19 21:20:38 +0800

    搭建html结构

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
 <template>
-<!--  <img alt="Vue logo" src="./assets/logo.png" />-->
-<!--  <HelloWorld msg="Hello Vue 3.0 + Vite" />-->
-  <div>导航栏
-    | <router-link to="/">Frank</router-link>
-    | <router-link to="/xxx">Frank2</router-link>
-  </div>
-  <hr/>
   <router-view/>
 </template>
 
