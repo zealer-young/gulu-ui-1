e0ffa069dfbfacf0710a40ec0705c0816de6bffd
Ethan <nightwish.ethan@gmail.com>
2024-02-26 00:15:17 +0800

    动画流畅了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -33,6 +33,7 @@ span{
   width: $h2;
   background:white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 button.checked {
   background: blue;
@@ -40,5 +41,7 @@ button.checked {
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
 }
-
+button:focus {
+  outline: none;
+}
 </style>
