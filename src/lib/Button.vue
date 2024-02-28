2f360b3eb684d3ff0f0df0d191293b856e89906d
Ethan <nightwish.ethan@gmail.com>
2024-02-28 09:48:33 +0800

    添加 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,23 +1,18 @@
 <template>
-  <div :size="size">
-    <button v-bind="rest">
-      <slot />
-    </button>
-  </div>
+  <button class="gulu-button" :class="`theme-${theme}`">
+    <slot />
+  </button>
 </template>
 <script lang="ts">
 export default {
-  inheritAttrs: false,
   props: {
-  },
-  setup(props, context) {
-    const { size, ...rest } = context.attrs;
-    return { size, rest };
-  },
-};
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
-div {
-  border: 1px solid red;
-}
+
 </style>
