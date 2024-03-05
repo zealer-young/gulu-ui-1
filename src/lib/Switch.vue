8ee1bc9c77a3a018728be2a9269fbf9929813b84
Ethan <nightwish.ethan@gmail.com>
2024-03-05 17:00:48 +0800

    @use 'sass:math'; border-radius: math.div($h, 2)

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -19,10 +19,12 @@ export default {
 </script>
 
 <style lang="scss">
+@use 'sass:math';
+
 $h: 22px;
 $h2: $h - 4px;
 .gulu-switch {
-  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: math.div($h, 2); position: relative;
   > span {
     position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
